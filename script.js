81c5b678d9dce5472488ff1361e5b6a0d733a764
Mark Taylor <mtaylor_001@hotmail.com>
2023-04-26 18:48:58 -0230

    Worked through logic on text file, added ids

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log("blue=10")
+const destroyer = [a1, a2];
+const submarine = [b1, b2, b3];
+
+const selection = Math.floor(Math.random() * 99) + 1
+
+let isValidChoice; 
+if(selection >= 1 && selection <= 100) {
+    isValidChoice = true;
+} else {
+    isValidChoice = false;
+}
+
+
+const a1 = document.getElementById("a1")
+
+
+const a1 = document.getElementById("a1")
+const a1 = document.getElementById("a1")
+const a1 = document.getElementById("a1")
+const a1 = document.getElementById("a1")
+const a1 = document.getElementById("a1")
+const a1 = document.getElementById("a1")
+const a1 = document.getElementById("a1")
+const a1 = document.getElementById("a1")
+const a1 = document.getElementById("a1")
+const a1 = document.getElementById("a1")
+const a1 = document.getElementById("a1")
+const a1 = document.getElementById("a1")
+const a1 = document.getElementById("a1")
+const a1 = document.getElementById("a1")
+const a1 = document.getElementById("a1")
+const a1 = document.getElementById("a1")
+const a1 = document.getElementById("a1")
+const a1 = document.getElementById("a1")
+const a1 = document.getElementById("a1")
+const a1 = document.getElementById("a1")
+const a1 = document.getElementById("a1")
+//same thing below, both grab an id'd item from html
+const cell1 = document.querySelector("#a1")
+
+const destroyer = [a1, a2];
+const destroyer = [a1, a2];
+const destroyer = [a1, a2];
