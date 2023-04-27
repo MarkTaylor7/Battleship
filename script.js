7be3070f1f23b5161227fb93b2f28eb889025321
Mark Taylor <mtaylor_001@hotmail.com>
2023-04-27 12:47:02 -0230

    Removed header bars for rows and columns and individually labelled each cell instead Assigned id to each tile and began assigning const for each Changed tile font to Impact font family

--- script.js
+++ script.js
@@ -12,28 +12,96 @@ if(selection >= 1 && selection <= 100) {
 
 
 const a1 = document.getElementById("a1")
+const b1 = document.getElementById("b1")
+const c1 = document.getElementById("c1")
+const d1 = document.getElementById("d1")
+const e1 = document.getElementById("e1")
+const f1 = document.getElementById("f1")
+const g1 = document.getElementById("g1")
+const h1 = document.getElementById("h1")
+const i1 = document.getElementById("i1")
+const j1 = document.getElementById("j1")
+
+const a2 = document.getElementById("a2")
+const b2 = document.getElementById("b2")
+const c2 = document.getElementById("c2")
+const d2 = document.getElementById("d2")
+const e2 = document.getElementById("e2")
+const f2 = document.getElementById("f2")
+const g2 = document.getElementById("g2")
+const h2 = document.getElementById("h2")
+const i2 = document.getElementById("i2")
+const j2 = document.getElementById("j2")
+
+const a3 = document.getElementById("a3")
+const b3 = document.getElementById("b3")
+const c3 = document.getElementById("c3")
+const d3 = document.getElementById("d3")
+const e3 = document.getElementById("e3")
+const f3 = document.getElementById("f3")
+const g3 = document.getElementById("g3")
+const h3 = document.getElementById("h3")
+const i3 = document.getElementById("i3")
+const j3 = document.getElementById("j3")
+
+const a4 = document.getElementById("a4")
+const b4 = document.getElementById("b4")
+const c4 = document.getElementById("c4")
+const d4 = document.getElementById("d4")
+const e4 = document.getElementById("e4")
+const f4 = document.getElementById("f4")
+const g4 = document.getElementById("g4")
+const h4 = document.getElementById("h4")
+const i4 = document.getElementById("i4")
+const j4 = document.getElementById("j4")
+
+const a5 = document.getElementById("a5")
+const b5 = document.getElementById("b5")
+const c5 = document.getElementById("c5")
+const d5 = document.getElementById("d5")
+const e5 = document.getElementById("e5")
+const f5 = document.getElementById("f5")
+const g5 = document.getElementById("g5")
+const h5 = document.getElementById("h5")
+const i5 = document.getElementById("i5")
+const j5 = document.getElementById("j5")
+
+const a6 = doucment.getElementById("a6")
+const b6 = document.getElementById("b6")
+const c6 = document.getElementById("c6")
+const d6 = document.getElementById("d6")
+const e6 = document.getElementById("e6")
+const f6 = document.getElementById("f6")
+const g6 = document.getElementById("g6")
+const h6 = document.getElementById("h6")
+const i6 = document.getElementById("i6")
+const j6 = document.getElementById("j6")
 
 
-const a1 = document.getElementById("a1")
-const a1 = document.getElementById("a1")
-const a1 = document.getElementById("a1")
-const a1 = document.getElementById("a1")
-const a1 = document.getElementById("a1")
-const a1 = document.getElementById("a1")
-const a1 = document.getElementById("a1")
-const a1 = document.getElementById("a1")
-const a1 = document.getElementById("a1")
-const a1 = document.getElementById("a1")
-const a1 = document.getElementById("a1")
-const a1 = document.getElementById("a1")
-const a1 = document.getElementById("a1")
-const a1 = document.getElementById("a1")
-const a1 = document.getElementById("a1")
-const a1 = document.getElementById("a1")
-const a1 = document.getElementById("a1")
-const a1 = document.getElementById("a1")
-const a1 = document.getElementById("a1")
-const a1 = document.getElementById("a1")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const a1 = document.getElementById("a1")
 //same thing below, both grab an id'd item from html
 const cell1 = document.querySelector("#a1")
