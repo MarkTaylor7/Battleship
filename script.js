d92632a81074345ae2e90174364c1f74626c5d68
Mark Taylor <mtaylor_001@hotmail.com>
2023-05-05 12:18:22 -0230

    Renamed consts from ship names to item1, item2, item3. Actual items will be determined later and names will be updated. Removed consts from cells that no longer exist, not that grid is 8x8.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const destroyer = [a1, a2];
-const submarine = [b1, b2, b3];
-const battleship = [c1, c2, c3];
+const item1 = [a1, a2];
+const item2 = [b1, b2, b3];
+const item3 = [c1, c2, c3];
 
 const selection = Math.floor(Math.random() * 99) + 1
 
@@ -24,8 +24,6 @@ const e1 = document.getElementById("e1")
 const f1 = document.getElementById("f1")
 const g1 = document.getElementById("g1")
 const h1 = document.getElementById("h1")
-const i1 = document.getElementById("i1")
-const j1 = document.getElementById("j1")
 
 const a2 = document.getElementById("a2")
 const b2 = document.getElementById("b2")
@@ -35,8 +33,6 @@ const e2 = document.getElementById("e2")
 const f2 = document.getElementById("f2")
 const g2 = document.getElementById("g2")
 const h2 = document.getElementById("h2")
-const i2 = document.getElementById("i2")
-const j2 = document.getElementById("j2")
 
 const a3 = document.getElementById("a3")
 const b3 = document.getElementById("b3")
@@ -46,8 +42,6 @@ const e3 = document.getElementById("e3")
 const f3 = document.getElementById("f3")
 const g3 = document.getElementById("g3")
 const h3 = document.getElementById("h3")
-const i3 = document.getElementById("i3")
-const j3 = document.getElementById("j3")
 
 const a4 = document.getElementById("a4")
 const b4 = document.getElementById("b4")
@@ -57,8 +51,6 @@ const e4 = document.getElementById("e4")
 const f4 = document.getElementById("f4")
 const g4 = document.getElementById("g4")
 const h4 = document.getElementById("h4")
-const i4 = document.getElementById("i4")
-const j4 = document.getElementById("j4")
 
 const a5 = document.getElementById("a5")
 const b5 = document.getElementById("b5")
@@ -68,8 +60,6 @@ const e5 = document.getElementById("e5")
 const f5 = document.getElementById("f5")
 const g5 = document.getElementById("g5")
 const h5 = document.getElementById("h5")
-const i5 = document.getElementById("i5")
-const j5 = document.getElementById("j5")
 
 const a6 = doucment.getElementById("a6")
 const b6 = document.getElementById("b6")
@@ -79,8 +69,6 @@ const e6 = document.getElementById("e6")
 const f6 = document.getElementById("f6")
 const g6 = document.getElementById("g6")
 const h6 = document.getElementById("h6")
-const i6 = document.getElementById("i6")
-const j6 = document.getElementById("j6")
 
 const a7 = document.getElementById("a7")
 const b7 = document.getElementById("b7")
@@ -90,8 +78,6 @@ const e7 = document.getElementById("e7")
 const f7 = document.getElementById("f7")
 const g7 = document.getElementById("g7")
 const h7 = document.getElementById("h7")
-const i7 = document.getElementById("i7")
-const j7 = document.getElementById("j7")
 
 const a8 = document.getElementById("a8")
 const b8 = document.getElementById("b8")
@@ -101,34 +87,6 @@ const e8 = document.getElementById("e8")
 const f8 = document.getElementById("f8")
 const g8 = document.getElementById("g8")
 const h8 = document.getElementById("h8")
-const i8 = document.getElementById("i8")
-const j8 = document.getElementById("j8")
-
-const a9 = document.getElementById("a9")
-const b9 = document.getElementById("b9")
-const c9 = document.getElementById("c9")
-const d9 = document.getElementById("d9")
-const e9 = document.getElementById("e9")
-const f9 = document.getElementById("f9")
-const g9 = document.getElementById("g9")
-const h9 = document.getElementById("h9")
-const i9 = document.getElementById("i9")
-const j9 = document.getElementById("j9")
-
-const a10 = document.getElementById("a10")
-const b10 = document.getElementById("b10")
-const c10 = document.getElementById("c10")
-const d10 = document.getElementById("d10")
-const e10 = document.getElementById("e10")
-const f10 = document.getElementById("f10")
-const g10 = document.getElementById("g10")
-const h10 = document.getElementById("h10")
-const i10 = document.getElementById("i10")
-const j10 = document.getElementById("j10")
 
 //same thing below, both grab an id'd item from html
 const cell1 = document.querySelector("#a1")
-
-const destroyer = [a1, a2];
-const destroyer = [a1, a2];
-const destroyer = [a1, a2];
