2b8831d7d2e4a5d9e02cc3215cc3e506c4d7b9f5
Mark Taylor <mtaylor_001@hotmail.com>
2023-08-14 16:16:12 -0230

    Changes on 08/14/23 Added missing semi-colons

--- script.js
+++ script.js
@@ -480,8 +480,8 @@ const allSquares = [a1, b1, c1, d1, e1, f1, g1, h1, a2, b2, c2, d2, e2, f2, g2, 
     a7, b7, c7, d7, e7, f7, g7, h7, a8, b8, c8, d8, e8, f8, g8, h8 
 ];
 
-const column1 = [a1, a2, a3, a4, a5, a6, a7, a8]
-const column8 = [h1, h2, h3, h4, h5, h6, h7, h8]
+const column1 = [a1, a2, a3, a4, a5, a6, a7, a8];
+const column8 = [h1, h2, h3, h4, h5, h6, h7, h8];
 
 //Treasures
 class Treasure {
